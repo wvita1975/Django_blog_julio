0b7f391d30879f5d946d9439d5f25d4e13a461ad
Nicole Bahri <bahrinicole1@gmail.com>
2023-07-01 11:37:45 -0400

    üpdate_post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostListView , PostDetailView
+from .views import PostListView , PostDetailView, PostUpdateView, PostCreateView 
 
 urlpatterns = [
+    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post_update"),
     path('', PostListView.as_view(), name="home"),
     path('post/<int:pk>', PostDetailView.as_view(), name="detail_view"),
+    path('post/new',PostCreateView.as_view(), name=''),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView , CreateView
 from .models import Post
 
 # Create your views here.
@@ -11,4 +12,12 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'detail_view.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'Post_new.html'
     
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title', 'body']
