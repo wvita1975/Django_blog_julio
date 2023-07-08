88c471538d56189320685d8da857d7f19d3855a7
Nicole Bahri <bahrinicole1@gmail.com>
2023-07-08 09:54:13 -0400

    Post delete y registration :)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView , PostDetailView, PostUpdateView, PostCreateView 
+from .views import PostListView , PostDetailView, PostUpdateView, PostCreateView, PostDeleteView
 
 urlpatterns = [
     path("post/<int:pk>/update", PostUpdateView.as_view(), name="post_update"),
     path('', PostListView.as_view(), name="home"),
     path('post/<int:pk>', PostDetailView.as_view(), name="detail_view"),
-    path('post/new',PostCreateView.as_view(), name=''),
+    path('post/new',PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from django.views.generic.edit import UpdateView , CreateView
+from django.views.generic.edit import UpdateView , CreateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -16,8 +17,14 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     model = Post
     template_name = 'Post_new.html'
+    fields = ['title', 'author', 'body']
     
 class PostUpdateView(UpdateView):
     model = Post
     template_name = 'post_update.html'
     fields = ['title', 'body']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name= "post_delete.html"
+    success_url= reverse_lazy('home')
